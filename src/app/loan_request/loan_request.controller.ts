0826f5c5079e59915f959a91909209a8dd464d37
nhao2003 <nhathaodev@gmail.com>
2024-06-13 00:34:24 +0700

    feat: Update loan_request.controller.ts
    This commit updates the loan_request.controller.ts file by adding type annotations to the payload parameters in the controller methods. This change improves the type safety and readability of the code.

--- src/app/loan_request/loan_request.controller.ts
+++ src/app/loan_request/loan_request.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateRpcPayload } from 'src/common/decorators';
 import { LoanRequestService } from './loan_request.service';
+import { RpcPayload } from 'src/common/types/rpc_payload.type';
 
 @Controller('loan-request')
 @ApiTags('Loan Request')
@@ -10,42 +11,43 @@ export class LoanRequestController {
   constructor(private readonly loanRequestService: LoanRequestService) {}
 
   @Post()
-  createLoanRequest(@CreateRpcPayload() payload) {
+  createLoanRequest(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.createLoanRequest(payload);
   }
 
   @Get('sent')
-  getSentLoanRequests(@CreateRpcPayload() payload) {
+  getSentLoanRequests(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.getSentLoanRequests(payload);
   }
 
   @Get('received')
-  async getReceivedLoanRequests(@CreateRpcPayload() payload) {
+  async getReceivedLoanRequests(@CreateRpcPayload() payload: RpcPayload) {
     return await this.loanRequestService.getReceivedLoanRequests(payload);
   }
 
   @Post(':id/approve')
-  async approveLoanRequest(@CreateRpcPayload() payload) {
+  async approveLoanRequest(@CreateRpcPayload() payload: RpcPayload) {
+    console.log('approveLoanRequest', payload);
     return this.loanRequestService.approveLoanRequest(payload);
   }
 
   @Post(':id/reject')
-  rejectLoanRequest(@CreateRpcPayload() payload) {
+  rejectLoanRequest(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.rejectLoanRequest(payload);
   }
 
   @Post(':id/cancel')
-  cancelLoanRequest(@CreateRpcPayload() payload) {
+  cancelLoanRequest(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.cancelLoanRequest(payload);
   }
 
   @Post(':id/pay')
-  payLoanRequest(@CreateRpcPayload() payload) {
+  payLoanRequest(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.payLoanRequest(payload);
   }
 
   @Post(':id/mark-paid')
-  markLoanRequestPaid(@CreateRpcPayload() payload) {
+  markLoanRequestPaid(@CreateRpcPayload() payload: RpcPayload) {
     return this.loanRequestService.markAsPaid(payload);
   }
 }
